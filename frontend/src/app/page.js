68f3e30996eff1330a0ac80e574664c37452d777
mohamedwaleed77 <139256760+mohamedwaleed77@users.noreply.github.com>
2025-04-08 15:13:54 +0200

    frontend

---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl text-black">Hope I can finish this fast</h1>
+    </div>
+  );
+}
