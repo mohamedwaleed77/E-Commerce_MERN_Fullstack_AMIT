8387da32e0ad4fddb1f8435747c164a0fa81c898
mohamedwaleed77 <139256760+mohamedwaleed77@users.noreply.github.com>
2025-04-08 18:18:54 +0200

    frontend

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-black">Hope I can finish this fast</h1>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>test</h1>
     </div>
   );
 }
